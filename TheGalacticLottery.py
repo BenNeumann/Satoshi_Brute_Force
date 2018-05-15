15d1ef3cef877c5b2c40cdfd21fc5e1529f4092c
BenNeumann <35029014+BenNeumann@users.noreply.github.com>
2018-05-15 19:30:39 +0300

    Add files via upload

---
+++ TheGalacticLottery.py
@@ -0,0 +1,177 @@
+
+print('********************************************************************************')
+print('MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM')
+print('MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMO$$777IIII777$$ZZZ8MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM')
+print('MMMMMMMMMMMMMMMMMMMMMMMMMMO$7?==?I7$77$7777777777$ZZONMMMMMMMMMMMMMMMMMMMMMMMMMM')
+print('MMMMMMMMMMMMMMMMMMMMMMNZ7?~=7$$$$$$$$$$777777777$$77$$ZONMMMMMMMMMMMMMMMMMMMMMMM')
+print('MMMMMMMMMMMMMMMMMMMMO$?:=7$$$$$$$$$$$$$7777777777777$$$$ZO8MMMMMMMMMMMMMMMMMMMMM')
+print('MMMMMMMMMMMMMMMMMMO$~,7$$$$$$$$$$$$$$$$$777777777777777$$$ZO8MMMMMMMMMMMMMMMMMMM')
+print('MMMMMMMMMMMMMMMMM$+,$$$$$$$$$$$$$$$$$$$$$77777777777777777$$Z8MMMMMMMMMMMMMMMMMM')
+print('MMMMMMMMMMMMMMMM$:?$$$$$$$$$$$$$$$$$$$$$$$777777777777777777$ZODMMMMMMMMMMMMMMMM')
+print('MMMMMMMMMMMMMMD7,I$$$$$$$$$$$$$$$$$$$$$$$$$777777777777777777$Z8NMMMMMMMMMMMMMMM')
+print('MMMMMMMMMMMMMM7,$$$$$$$$$$$$$$$$$$$$$$$$$$$$777777777777777777$Z8NMMMMMMMMMMMMMM')
+print('MMMMMMMMMMMMM$:7$$$$$$$$$$$$$$$$$$$$$$$$$$$$7777777777777777777$ODMMMMMMMMMMMMMM')
+print('MMMMMMMMMMMMZ+?$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$777777777777777777$ZONMMMMMMMMMMMMM')
+print('MMMMMMMMMMMM$~$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$77777777777777777$$ZDMMMMMMMMMMMMM')
+print('MMMMMMMMMMMZ+I$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$77777777777777777$Z8NMMMMMMMMMMMM')
+print('MMMMMMMMMMM$=$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$77777777777777777$ONMMMMMMMMMMMM')
+print('MMMMMMMMMMD$?$ZZZ$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$7$777777777777777$ODMMMMMMMMMMMM')
+print('MMMMMMMMMMO77ZZZOOOOZZ$$$$$$$$$$$$$$$$$$$$$$$$$$$7777777$$$$$$777$ZDMMMMMMMMMMMM')
+print('MMMMMMMMMMO77ZODNNNND88OZ$$$$$$$$$$$$$$$$$$$$$$$$$7$$$$O88DDDD8Z$$ZDMMMMMMMMMMMM')
+print('MMMMMMMMMMO$7ONNMMMMMMND8OZ$$$$$$$$$$$$$$$$$$$$$$$$$O8NNNMMMMMMN8ZZDMMMMMMMMMMMM')
+print('MMMMMMMMMMD$ZNMMMMMMMMMMMD8OZ$$$$$$$$$$$$$$$$$$$$$ODNNMMMMMMMMMMN8ZDMMMMMMMMMMMM')
+print('MMMMMMMMMMMZ8NMMMMMMMMMMMMM8OZ$$$$$$$$$$$$$$$$$$$ONNMMMMMMMMMMMMM8ZNMMMMMMMMMMMM')
+print('MMMMMMMMMMMZ8MMMMMMMMMMMMMMM8OZ$$$$$$$$$$$$$$$$$8NMMMMMMMMMMMMMMM8ZNMMMMMMMMMMMM')
+print('MMMMMMMMMMMOONMMMMMMMMMMMMMMMNOZ$$$$$$$$$$$$$$$ONNMMMMMMMMMMMMMMMZ8MMMMMMMMMMMMM')
+print('MMMMMMMMMMMNZNMMMMMMMMMMMMMMMMM8Z$$$$$$$$$$$$$8NNMMMMMMMMMMMMMMM87NMMMMMMMMMMMMM')
+print('MMMMMMMMMMMMOZNMMMMMMMMMMMMMMMMMOZ$$$$$$$$$$$8NNMMMMMMMMMMMMMMMM$OMMMMMMMMMMMMMM')
+print('MMMMMMMMMMMMNZZDMMMMMMMMMMMMMMMMNOZ$$$$$$$$$ONNMMMMMMMMMMMMMMMM$+NMMMMMMMMMMMMMM')
+print('MMMMMMMMMMMMMOZ$8NMMMMMMMMMMMMMMM8Z$$$$$$$$ODNMMMMMMMMMMMMMMM$I:DNMMMMMMMMMMMMMM')
+print('MMMMMMMMMMMMMMO$$O8DMMMMMMMMMMMMMMOZ$$$$$$ZDNMMMMMMMMMMMMMMD7==8NMMMMMMMMMMMMMMM')
+print('MMMMMMMMMMMMMM8Z7$ZO8DMMMMMMMMMMMMNO$$$$$$8NMMMMMMMMMMMMMO7=,7ODNMMMMMMMMMMMMMMM')
+print('MMMMMMMMMMMMMMMOZ$$$ZO8DMMMMMMMMMMMO$I$$$ODNMMMMMMMMMMMZ7~:7$Z8NMMMMMMMMMMMMMMMM')
+print('MMMMMMMMMMMMMMMMO$$$$$$ZO8DMMMMMMMMM$?$$$ODNMMMMMMMM$$=,+$$$$ONNMMMMMMMMMMMMMMMM')
+print('MMMMMMMMMMMMMMMMDO$$$$$$$$ZOO8DMMMMN7=$$$O8NMMMMO$I~:+$$$$$$ODNMMMMMMMMMMMMMMMMM')
+print('MMMMMMMMMMMMMMMMM8Z$$$$$$$$$$$ZZOZZ7=I$$$$$$$7I=~=7$$$$$$$$Z8NMMMMMMMMMMMMMMMMMM')
+print('MMMMMMMMMMMMMMMMMMOZ$$$$$$$$$$$$$777$$$$$$$$$$$$$$$$$$$$$$$8NMMMMMMMMMMMMMMMMMMM')
+print('MMMMMMMMMMMMMMMMMMNOZ$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ODNMMMMMMMMMMMMMMMMMMM')
+print('MMMMMMMMMMMMMMMMMMMDOZ$$$$$$$$$$$$Z888OO8DD8Z$$$$$$$$$$$$ODNMMMMMMMMMMMMMMMMMMMM')
+print('MMMMMMMMMMMMMMMMMMMM8O$$$$$$$$$$$Z8NMNO8NMMD$$$$$$$$$$$$ODNMMMMMMMMMMMMMMMMMMMMM')
+print('MMMMMMMMMMMMMMMMMMMMM8O$$$$$$$$$$ZODDMM$MM87?$$$$$$$$$$ODNMMMMMMMMMMMMMMMMMMMMMM')
+print('MMMMMMMMMMMMMMMMMMMMMM8O$$$$$$$$$$ZZZZ$I7?=+$$$$$$$$$$ZDNMMMMMMMMMMMMMMMMMMMMMMM')
+print('MMMMMMMMMMMMMMMMMMMMMMM8Z$$$$$$$$$ZZZZ$7$$$$$$$$$$$$ZZDNMMMMMMMMMMMMMMMMMMMMMMMM')
+print('MMMMMMMMMMMMMMMMMMMMMMMM8Z$$$$$$ZZDNNNNDDDD8OZZ$$$$ZODNMMMMMMMMMMMMMMMMMMMMMMMMM')
+print('MMMMMMMMMMMMMMMMMMMMMMMMM8Z$$$$$Z8DNMMMMMMMN8Z$$$$ZODNMMMMMMMMMMMMMMMMMMMMMMMMMM')
+print('MMMMMMMMMMMMMMMMMMMMMMMMMM8O$$$$$$$7777777II$$$$$Z8NNMMMMMMMMMMMMMMMMMMMMMMMMMMM')
+print('MMMMMMMMMMMMMMMMMMMMMMMMMMM8OZ$$$$$$$$$$$$$$$$$$Z8NMMMMMMMMMMMMMMMMMMMMMMMMMMMMM')
+print('MMMMMMMMMMMMMMMMMMMMMMMMMMMM8OZZ$$$$$$$$$$$$$$$ZDNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM')
+print('MMMMMMMMMMMMMMMMMMMMMMMMMMMMMM8OZ$$$$$$$$$$$$Z8NNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM')
+print('MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM88OZ$$$$$$$$ZODNNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM')
+print('MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMD8OOZZZZO8DNNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM')
+print('MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNNDDDDNNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM')
+print('MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM')
+print('MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM')
+print('********************************************************************************')
+
+#-----------------------Import-----------------------
+import hashlib
+import base58
+import math
+from os import urandom
+from keyDatabase  import satoshiKeys
+from random import randint
+
+#-----------------------Global Vars-----------------------
+blockSize = 200
+x=0
+Pcurve = 2**256 - 2**32 - 2**9 - 2**8 - 2**7 - 2**6 - 2**4 -1 
+N=0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141 
+Acurve = 0; Bcurve = 7 
+Gx = 55066263022277343669578718895168534326250603453777594175500187360389116729240
+Gy = 32670510020758816978083085130507043184471273380659243275938904335757337482424
+GPoint = (Gx,Gy) 
+blocklimit = (Pcurve)/blockSize
+blocklimitHE = hex(blocklimit).split('x')[-1]
+bytesr = len(blocklimitHE)-1
+modbytes = bytesr % 2
+blockNumber =0
+
+#-----------------------Elliptic Curve Secp256k-----------------------
+
+def modinv(a,n=Pcurve): 
+    lm, hm = 1,0
+    low, high = a%n,n
+    while low > 1:
+        ratio = high/low
+        nm, new = hm-lm*ratio, high-low*ratio
+        lm, low, hm, high = nm, new, lm, low
+    return lm % n
+
+def ECadd(a,b):
+    LamAdd = ((b[1]-a[1]) * modinv(b[0]-a[0],Pcurve)) % Pcurve
+    x = (LamAdd*LamAdd-a[0]-b[0]) % Pcurve
+    y = (LamAdd*(a[0]-x)-a[1]) % Pcurve
+    return (x,y)
+
+def ECdouble(a): 
+    Lam = ((3*a[0]*a[0]+Acurve) * modinv((2*a[1]),Pcurve)) % Pcurve
+    x = (Lam*Lam-2*a[0]) % Pcurve
+    y = (Lam*(a[0]-x)-a[1]) % Pcurve
+    return (x,y)
+
+def EccMultiply(GenPoint,ScalarHex): 
+    if ScalarHex == 0 or ScalarHex >= N: hashing();
+    ScalarBin = str(bin(ScalarHex))[2:]
+    Q=GenPoint
+    for i in range (1, len(ScalarBin)): 
+        Q=ECdouble(Q); 
+        if ScalarBin[i] == "1":
+            Q=ECadd(Q,GenPoint);  
+    return (Q)
+
+#-----------------------Find Key in Database Function-----------------------
+def find(q,ss,key):
+    global y,n,x
+    if q in ss:
+            lr = str(q) + "      Match: " + str(key)
+            y +=1
+            x +=1
+    else:
+        lr = str(q) + "      No Match: " + str(key)
+        n += 1
+    return(lr)
+
+    
+#-----------------------Random Function-----------------------
+def start1():
+    global blockSize, blocklimit, blockNumber, blocklimitHE, bytesr,  modbytes
+    if modbytes>0:
+    	getf = int(blocklimitHE[0],16)
+    	getfhex = hex(randint(0,getf)).split('x')[-1]
+    	random_bytes = str(getfhex)+urandom(bytesr/2).encode('hex')
+    else:
+    	random_bytes1 = urandom(bytesr/2)
+    	random_bytes = random_bytes1.encode('hex')
+    userInput = int(random_bytes,16)
+    if userInput>blocklimit:
+    	start1()
+    else:
+        blockNumber = userInput
+        return blockNumber
+ 
+#-----------------------main function-----------------------
+def hashing():
+	global blockSize, blockNumber,y,n,x
+	while (x<1):
+		start1()
+		x=0
+		n=0
+		y=0
+		startRange = blockNumber*blockSize+1
+		endRange =startRange+blockSize
+		for key in range(startRange,endRange):
+			PublicKey = EccMultiply(GPoint,key)
+			uncompress = "04" + "%064x" % PublicKey[0] + "%064x" % PublicKey[1];
+			uncompressbin = str(uncompress).decode('hex')
+			outputsha256 = hashlib.new('sha256',uncompressbin).digest()
+			outputripemd160 = hashlib.new('ripemd160',outputsha256).digest()
+			q=outputripemd160.encode('hex')
+			print find(q,satoshiKeys,key)
+			if x>0:
+				keyHex = hex(key).split('x')[-1];
+				keyHexf = '80'+('0'*(64-len(keyHex)))+str(keyHex)
+				decodewin = str(keyHexf).decode('hex');
+				checksumSHAa = hashlib.new('sha256',decodewin).digest()
+				checksumSHAb = hashlib.new('sha256',checksumSHAa).digest()
+				checksumwin = checksumSHAb.encode('hex')
+				preWIF = str(keyHexf)+str(checksumwin[0:8])
+				decodewin2 = str(preWIF).decode('hex')
+				encodeWIF = base58.b58encode(decodewin2)
+				print encodeWIF
+				break
+		print ""
+
+#-----------------------Triger-----------------------
+hashing()
+
+
